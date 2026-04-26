c4af5b0d5276ad338414b76ae954a702dd7d46a4
Андрій <andriystetsula2344gmail.com@MacBook-Pro-Andrij.local>
2026-04-26 10:55:43 +0300

    git add solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,13 @@
-// export const Person = ({ person }) => ();
+export const Person = ({
+  person: { name, age, sex, isMarried, partnerName },
+}) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {name}</h2>
+    {age !== undefined && <p className="Person__age">I am 37</p>}
+    <p className="Person__partner">
+      {isMarried
+        ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+        : 'not married'}
+    </p>
+  </section>
+);
